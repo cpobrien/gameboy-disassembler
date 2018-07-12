90af2a277c59718deddaf4ee2f637ae55e38db03
Connor O'Brien <connorpadraigobrien@gmail.com>
2018-07-11 21:30:51 -0700

    攰

--- index.ts
+++ index.ts
@@ -6,70 +6,124 @@ const toWord = (lo: number, hi: number): number => hi << 8 | lo;
 const toHex = (num: number): string => num.toString(16);
 const toSigned = (num: number): number => ((~num & 0xFF) - 1) * ((num & 0x80) === 0 ? 1 : -1);
 
-type ParsingFunction = {(code: Uint8Array): string};
+const CB_CYCLE_TABLE: number[] = [
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8,
+    8, 8, 8, 8, 8, 8, 16, 8
+];
+
+
+type ParseResponse = {
+    text: string,
+    cycles: number
+};
+
+type ParsingFunction = {(code: Uint8Array): ParseResponse};
 
 const ret: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     switch (op) {
-        case 0xC0: return `\tRET NZ`;
-        case 0xC8: return `\tRET Z`;
-        case 0xC9: return `\tRET`;
-        case 0xD0: return `\tRET NC`;
-        case 0xD8: return `\tRET C`;
-        case 0xD9: return `\tRETI`;
-        default: return `???`;
+        case 0xC0: return {text: `\tRET NZ`, cycles: cycles};
+        case 0xC8: return {text: `\tRET Z`, cycles: cycles};
+        case 0xC9: return {text: `\tRET`, cycles: cycles};
+        case 0xD0: return {text: `\tRET NC`, cycles: cycles};
+        case 0xD8: return {text: `\tRET C`, cycles: cycles};
+        case 0xD9: return {text: `\tRETI`, cycles: cycles};
+        default: return {text: `???`, cycles: cycles};
     }
 }
 
 const addReg: ParsingFunction = code => {
     const op: number = code[0];
-    return `\tADD A,${['B', 'C', 'D', 'E', 'H', 'L', '(HL)', 'A'][op & 0x7]}`
+    const cycles: number = CB_CYCLE_TABLE[op];
+    return {text: `\tADD A,${['B', 'C', 'D', 'E', 'H', 'L', '(HL)', 'A'][op & 0x7]}`, cycles: cycles}
 }
 const add: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     if ((op >> 4) === 8) return addReg(code);
-    return '???';
+    return {text: '???', cycles: cycles};
 }
 
-const rla: ParsingFunction = code => `\tRLA`;
+const rla: ParsingFunction = code => {
+    const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
+  return {text: `\tRLA`, cycles: cycles};
+}
 
 const rl: ParsingFunction = code => {
     const op: number = code[0];
-    return `\tRL ${['B', 'C', 'D', 'E', 'H', 'L', '(HL)', 'A'][op & 0x7]}`;
+    const cycles: number = CB_CYCLE_TABLE[op];
+    return {text: `\tRL ${['B', 'C', 'D', 'E', 'H', 'L', '(HL)', 'A'][op & 0x7]}`, cycles: cycles};
 }
 
 const ldWordToA: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     const word: string = toHex(toWord(code[1], code[2]));
-    if (op === 0xEA) return `\tLD ($${word}),A`;
-    else return `\tLD A,($${word})`;
+    if (op === 0xEA) return {text: `\tLD ($${word}),A`, cycles: cycles};
+    else return {text: `\tLD A,($${word})`, cycles: cycles};
 }
 
 const ldWord: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     const loc: string = ['BC', 'DE', 'HL', 'SP'][op >> 4];
     const pos: string = toHex(toWord(code[1], code[2]));
-    return `\tLD ${loc},$${pos}`;
+    return {text: `\tLD ${loc},$${pos}`, cycles: cycles};
 }
 
 const ldCombo: ParsingFunction = code => {
     const op : number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     const loc: string = ['(BC)', '(DE)', '(HL+)', '(HL-)'][op >> 4];
-    if ((op & 0xF) == 0x2) return `\tLD ${loc},A`;
-    else return `\tLD A,${loc}`;
+    if ((op & 0xF) == 0x2) return {text: `\tLD ${loc},A`, cycles: cycles};
+    else return {text: `\tLD A,${loc}`, cycles: cycles};
 }
 
 const sub: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     var val: number | string = ['B', 'C', 'D', 'E', 'H', 'L', '(HL)', 'A'][op & 0x7];
     if (op == 0xD6) {
         val = code[1];
     }
-    return `\tSUB ${val}`;
+    return {text: `\tSUB ${val}`, cycles: cycles};
 }
 
 const ldReg: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     const regTable: string[] = ['B', 'C', 'D', 'E', 'H', 'L', '(HL)', 'A'];
     const from: string = regTable[(op - 0x40) >> 3];
     const to: string = regTable[op & 7]
@@ -78,18 +132,21 @@ const ldReg: ParsingFunction = code => {
 
 const ldByte: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     const reg: string = ['B', 'C', 'D', 'E', 'H', 'L', '(HL)', 'A'][(op - 6) >> 3];
     return `\tLD ${reg},$${toHex(code[1])}`;
 }
 
 const ldC: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     if (op === 0xE2) return `\tLD ($FF00+C),A`;
     else return `\tLD A,$FF00+C)`;
 }
 
 const ld: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     if (op >> 4 < 4 && (op & 7) === 1) return ldWord(code);
     if (op >> 4 < 4 && (op & 7) === 6) return ldByte(code);
     if (op >> 4 < 4 && ((op & 7) === 2 || (op & 0xF) === 0xA)) return ldCombo(code);
@@ -107,6 +164,7 @@ const xor: ParsingFunction = code => '\tXOR A';
 const fail: ParsingFunction = code => "UNKNOWN";
 const bit: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     const reg: string = ['B', 'C', 'D', 'E', 'H', '(HL)', 'A'][op & 7];
     const num: number = (op - 0x40) >> 3;
 
@@ -115,58 +173,68 @@ const bit: ParsingFunction = code => {
 
 const decWord: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     const word: string = ['BC', 'DE', 'HL', 'SP'][op >> 4];
     return `\tDEC ${word}`;
 }
 
 const decReg: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     const reg: string = ['B', 'C', 'D', 'E', 'H', 'L', '(HL)', 'A'][(op - 5) >> 3];
     return `\tDEC ${reg}`
 }
 
 const dec: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     if ((op & 7) === 5) return decReg(code);
     else return decWord(code);
 }
 
 const push: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     const word: string = ['BC', 'DE', 'HL', 'SP'][(op >> 4) - 0xC];
     return `\tPUSH ${word}`;
 }
 
 const pop: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     const word: string = ['BC', 'DE', 'HL', 'SP'][(op >> 4) - 0xC];
     return `\tPOP ${word}`;
 }
 
 const incReg: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     const reg: string = ['B', 'C', 'D', 'E', 'H', 'L', '(HL)', 'A'][(op - 4) >> 3];
     return `\tINC ${reg}`
 }
 const incWord: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     const word: string = ['BC', 'DE', 'HL', 'SP'][op >> 4];
     return `\tINC ${word}`;
 }
 const inc: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     if ((op & 7) === 4) return incReg(code);
     else return incWord(code);
 }
 
 const ldh: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     if (op === 0xE0) return `\tLDH ($FF00+$${toHex(code[1])}),A`;
     else return `\tLDH A,($FF00+$${toHex(code[1])})`;
 }
 
 const call: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     let reg = '';
     switch (op) {
         case 0xC4: reg = 'NZ,'; break;
@@ -179,6 +247,7 @@ const call: ParsingFunction = code => {
 
 const cp: ParsingFunction = code => {
     const op: number = code[0];
+    const cycles: number = CB_CYCLE_TABLE[op];
     const reg: string = ['B', 'C', 'D', 'E', 'H', 'L', '(HL)', 'A'][op & 7];
     if (op === 0xFE) return `\tCP $${toHex(code[1])}`
     else return `\tCP ${reg}`;
@@ -217,41 +286,6 @@ const CB_TABLE: ParsingFunction[] = [
     fail, fail, fail, fail, fail, fail, fail, fail, // 0xE8
     fail, fail, fail, fail, fail, fail, fail, fail, // 0xF0
     fail, fail, fail, fail, fail, fail, fail, fail  // 0xF8
-]
-
-const CB_CYCLE_TABLE: number[] = [
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8,
-    8, 8, 8, 8, 8, 8, 16, 8
 ];
 
 const PARSE_TABLE: ParsingFunction[] = [
@@ -360,9 +394,10 @@ const INSTRUCTION_SIZE_TABLE: number[] = [
     0, 0, 3, 0, 0, 0, 2, 0  // 0xF8
 ];
 
-const disassemble = (program: Uint8Array): string => {
+const tick = (program: Uint8Array): string => {
     let instructions: string[] = [];
     let i = 0;
+    var cycle: number = 0;
     while (i < program.length) {
         var opcode: number = program[i];
         var size: number = INSTRUCTION_SIZE_TABLE[opcode];
@@ -377,22 +412,23 @@ const disassemble = (program: Uint8Array): string => {
             console.error(`fail at $${toHex(opcode)}`);
             break;
         }
-        const disassembled: string = parser(code);
-        instructions.push(`${disassembled}\t\t$${toHex(i)}`);
+        const response: ParseResponse = parser(code);
+        instructions.push(`${response.text}\t\t$${toHex(i)}\t${response.cycles} cycles`);
         if (i === 0xA7) {
             instructions.push('~~~Gameboy logo data, skipping ahead~~~');
             i = 0xE0;
             continue;
         }
         i += size;
+        cycle += cycleCount;
     }
     return instructions.reduce((a, b) => `${a}\n${b}`, "");
 }
 
-fs.readFile(PATH, (err, data: Buffer) => {
+fs.readFile(PATH, (err: any, data: any) => {
     if (err) {
         console.error(err);
         return;
     }
-    console.log(disassemble(data));
+    console.log(tick(data));
 });
